83d72930c60e18074035b6fc7911b91661c9add5
rubenstr <rubens.teixeira_terceiros@nhecotech.com>
2026-01-30 09:44:52 -0400

    ajustes nas rotas

--- RPPT--Registro-Publico-de-Pets-e-Tutores/src/app/app.routes.ts
+++ RPPT--Registro-Publico-de-Pets-e-Tutores/src/app/app.routes.ts
@@ -1,19 +1,31 @@
 import { Routes } from '@angular/router';
 
 export const APP_ROUTES: Routes = [
-    {
-        path: '',
-        redirectTo: '/login',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./components/login/login').then(l => l.Login)
-    },
-    {   path: 'pets',
-        loadComponent: () => import('./components/pet/pet').then(p => p.Pet)
-    },
-    {   path: 'tutors',
-        loadComponent: () => import('./components/tutor/tutor').then(t => t.Tutor)
-    }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login').then(l => l.Login)
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/dashboard/dashboard').then(d => d.Dashboard),
+    children: [
+      {
+        path: 'pets',
+        loadComponent: () =>
+          import('./components/pet/pet').then(p => p.Pet)
+      },
+      {
+        path: 'tutors',
+        loadComponent: () =>
+          import('./components/tutor/tutor').then(t => t.Tutor)
+      }
+    ]
+  }
 ];
