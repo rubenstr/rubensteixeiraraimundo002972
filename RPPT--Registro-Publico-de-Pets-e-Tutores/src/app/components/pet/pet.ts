42b2f3a34766ab2f5b2bc55e4a47745471d4f7d4
rubenstr <rubens.teixeira_terceiros@nhecotech.com>
2026-01-29 21:39:48 -0400

    ajustes lista pets

--- RPPT--Registro-Publico-de-Pets-e-Tutores/src/app/components/pet/pet.ts
+++ RPPT--Registro-Publico-de-Pets-e-Tutores/src/app/components/pet/pet.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, Directive } from '@angular/core';
+import { PetService } from '../../services/pet.service';
+import { PetListInterface } from '../../interfaces/pet.interfaces';
+import { CommonModule, JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-pet',
-  imports: [],
+  imports: [JsonPipe, CommonModule],
   standalone: true,
   templateUrl: './pet.html',
   styleUrl: './pet.css',
 })
-export class Pet {
+
+export class Pet implements AfterViewInit{
+  
+  constructor(private _petService: PetService) {}
+ 
+  petsList: PetListInterface[] = [];
+
+   ngAfterViewInit() {
+    this._petService.getPets().subscribe((data: PetListInterface[])=> {
+      console.log("****Data: ", data.content);
+      this.petsList  = data;
+    }) ;
+ 
+  }
+
+
+
+
 
 }
