fc4cc853ffcf70d3ee2218cb6d71aa79efb63fcd
rubenstr <rubens.teixeira_terceiros@nhecotech.com>
2026-01-30 09:45:42 -0400

    ajustes no carregamento dos componentes por meio do menu e routers

--- RPPT--Registro-Publico-de-Pets-e-Tutores/src/app/components/dashboard/dashboard.ts
+++ RPPT--Registro-Publico-de-Pets-e-Tutores/src/app/components/dashboard/dashboard.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MenuSuperior } from '../shared/menu-superior/menu-superior';
 import { Pet } from '../pet/pet';
 import { Tutor } from '../tutor/tutor';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,22 +11,26 @@ import { Tutor } from '../tutor/tutor';
   imports: [    
     CommonModule,
     MenuSuperior,
+    RouterOutlet,
     Pet,
     Tutor],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
-  
+ constructor(private readonly router: Router) {}
   activeTab = signal<'pets' | 'tutores'>('pets');
   filterText = signal('');
 
-  onTabChange(tab: 'pets' | 'tutores') {
-    this.activeTab.set(tab);
-    this.filterText.set('');
-  }
+onTabChange(tab: 'pets' | 'tutores') {
+  this.activeTab.set(tab);
+  this.filterText.set('');
+
+  this.router.navigate([tab === 'pets' ? '/pets' : '/tutors']);
+}
 
   onFilterChange(value: string) {
+    console.log('Filter changed to:', value);
     this.filterText.set(value);
   }
 
