18412bc015d03a71dde2e1d3af625b95d1d05309
rubenstr <rubens.teixeira_terceiros@nhecotech.com>
2026-01-30 22:46:01 -0400

    adicionar componente de paginacao

---
+++ RPPT--Registro-Publico-de-Pets-e-Tutores/src/app/components/shared/pagination/pagination.ts
@@ -0,0 +1,29 @@
+import { Component, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pagination',
+  imports: [],
+  templateUrl: './pagination.html',
+  styleUrl: './pagination.css',
+})
+export class Pagination {
+  page = input(0);
+  pageCount = input(0);
+  size = input(10);
+  sizeChange = output<number>();
+
+  pageChange = output<number>();
+
+  previous() {
+    if (this.page() > 0) {
+      this.pageChange.emit(this.page() - 1);
+    }
+  }
+
+  next() {
+    if (this.page() < this.pageCount() - 1) {
+      this.pageChange.emit(this.page() + 1);
+    }
+  }
+}
