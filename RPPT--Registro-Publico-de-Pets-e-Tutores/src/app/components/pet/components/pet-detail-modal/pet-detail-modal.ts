2e3f678f35e38873b0b0a90987e3412de687589b
rubenstr <rubens.teixeira_terceiros@nhecotech.com>
2026-01-31 22:14:48 -0400

    ajustes genericos

--- RPPT--Registro-Publico-de-Pets-e-Tutores/src/app/components/pet/components/pet-detail-modal/pet-detail-modal.ts
+++ RPPT--Registro-Publico-de-Pets-e-Tutores/src/app/components/pet/components/pet-detail-modal/pet-detail-modal.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { TutorService } from '../../../../services/tutor.service';
 
 @Component({
   selector: 'app-pet-detail-modal',
@@ -12,5 +13,37 @@ export class PetDetailModal {
   pet = input<any | null>(null);
   loading = input(false);
   close = output<void>();
+
+  expandedTutorId = signal<number | null>(null);
+  tutorDetail = signal<any | null>(null);
+  loadingTutor = signal(false);
+
+  constructor(private readonly tutorService: TutorService) { }
+
+  toggleTutor(tutorId: number) {
+    if (this.expandedTutorId() === tutorId) {
+      this.expandedTutorId.set(null);
+      return;
+    }
+
+    this.expandedTutorId.set(tutorId);
+
+    if (this.tutorDetail()?.id === tutorId) {
+      return;
+    }
+
+    this.loadingTutor.set(true);
+
+    this.tutorService.getTutorById(tutorId).subscribe({
+      next: tutor => {
+        this.tutorDetail.set(tutor);
+        this.loadingTutor.set(false);
+      },
+      error: () => {
+        this.tutorDetail.set(null);
+        this.loadingTutor.set(false);
+      }
+    });
+  }
 
 }
